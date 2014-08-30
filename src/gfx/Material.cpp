744ab255fbf6afb4cc26d6878a28319d57df0af9
Johan Rende <johan@rende.se>
2014-08-31 00:12:56 +0200

    Added specular intensity and exponent values to material

--- src/gfx/Material.cpp
+++ src/gfx/Material.cpp
@@ -2,7 +2,9 @@
 Material::Material() {
 
 }
-Material::Material(std::string name): name(name)
+Material::Material(std::string name):
+  name(name),
+  specular_intensity(5.0f),
+  specular_exponent(100.0f)
 {
-
 }
--- src/gfx/Material.hpp
+++ src/gfx/Material.hpp
@@ -2,15 +2,21 @@
 #define __HONDO_MATERIAL_H__
 class Material;
 #include <string>
+#define GLM_FORCE_RADIANS
 #include <glm/glm.hpp>
 #include <GL/glew.h>
 class Material {
   private:
   public:
     std::string name;
+
     GLuint diffuse_map;
     GLuint specular_map;
     GLuint normal_map;
+
+    float specular_intensity;
+    float specular_exponent;
+
     Material();
     Material(std::string name);
 };
