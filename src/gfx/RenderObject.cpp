0539d2730836de90db1322601af9eebe5da961e0
Johan Rende <johan@rende.se>
2014-08-31 00:20:49 +0200

    Made RenderObject transform operation order independent

--- src/gfx/RenderObject.cpp
+++ src/gfx/RenderObject.cpp
@@ -7,7 +7,11 @@
 
 RenderObject::RenderObject(std::shared_ptr<VertexArray> vertex_array, Mesh mesh):
   vertex_array(vertex_array),
-  mesh(mesh)
+  mesh(mesh),
+  model_matrix(),
+  pos(0, 0, 0),
+  scale_val(1, 1, 1),
+  rot()
 {
 }
 
@@ -17,12 +21,10 @@ void RenderObject::render() const {
 
 RenderObject::RenderObject(const RenderObject& other):
     vertex_array(other.vertex_array),
-    color(other.color),
     model_matrix(other.model_matrix),
     mesh(other.mesh)
 {
   std::cout << "RenderObject copy constructor called" << std::endl;
-
 }
 
 void RenderObject::bind_material(SimpleShader& simple_shader) const {
@@ -32,11 +34,27 @@ void RenderObject::bind_material(SimpleShader& simple_shader) const {
 }
 
 void RenderObject::translate(const glm::vec3& pos) {
+  //model_matrix = glm::translate(model_matrix, pos);
+  this->pos += pos;
+}
+
+void RenderObject::scale(const glm::vec3& scale) {
+  //model_matrix = glm::scale(model_matrix, scale);
+  this->scale_val *= scale;
+}
+
+void RenderObject::rotate(float angle, const glm::vec3& axis) {
+  rot = glm::rotate(rot, angle, axis);
+}
+
+const glm::mat4& RenderObject::get_model_matrix() {
+  model_matrix = glm::mat4(1.0f);
   model_matrix = glm::translate(model_matrix, pos);
+  model_matrix = glm::scale(model_matrix, scale_val);
+  model_matrix = model_matrix * glm::mat4_cast(rot);
+  return model_matrix;
 }
 
 void RenderObject::set_position(const glm::vec3& pos) {
-  model_matrix[3][0] = pos.x;
-  model_matrix[3][1] = pos.y;
-  model_matrix[3][2] = pos.z;
+  this->pos = pos;
 }
