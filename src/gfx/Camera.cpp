d535fc34498fa47253243c976b6c2b2001bb84bc
Johan Rende <johan@rende.se>
2014-08-31 00:10:29 +0200

    Negated camera position translation

--- src/gfx/Camera.cpp
+++ src/gfx/Camera.cpp
@@ -1,7 +1,6 @@
 #include "Camera.hpp"
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtx/quaternion.hpp>
-#include <glm/gtc/quaternion.hpp>
 #include <glm/gtx/rotate_vector.hpp>
 #include <iostream>
 
@@ -9,8 +8,9 @@ Camera::Camera():
   up({0, 1.0f, 0}),
   pos({0, 0, 0}),
   dir({0, 0, 1.0f}),
-  view_mat(glm::lookAt(pos, dir+pos, up))
-  {}
+  view_mat(glm::lookAt(pos, pos+dir, up))
+  {
+  }
 
 void Camera::translate(glm::vec3 pos) {
   this->pos += pos;
@@ -21,15 +21,15 @@ void Camera::rotate(float angle, glm::vec3 axis) {
   dir = glm::vec3(glm::vec4(0, 0, 1, 0.0f) * glm::mat4_cast(rot));
 }
 
-glm::mat4 Camera::get_view_mat() {
+const glm::mat4& Camera::get_view_mat() {
   view_mat = glm::mat4{};
-  view_mat = glm::mat4_cast(rot) * view_mat;
-  view_mat = glm::translate(view_mat, pos);
+  view_mat = view_mat * glm::mat4_cast(rot);
+  view_mat = glm::translate(view_mat, -pos);
   return view_mat;
 }
 
 void Camera::move_forward(float dist) {
-  glm::vec3 temp = dir;
+  glm::vec3 temp = -dir;
   temp.y = 0;
   temp = glm::normalize(temp);
   this->translate(dist*temp);
@@ -37,5 +37,5 @@ void Camera::move_forward(float dist) {
 
 void Camera::move_right(float dist) {
   glm::vec3 left = glm::cross(up, dir); 
-  this->translate(-dist*left);
+  this->translate(dist*left);
 }
--- src/gfx/Camera.hpp
+++ src/gfx/Camera.hpp
@@ -10,7 +10,7 @@ class Camera {
     glm::quat rot;
     glm::mat4 view_mat;
     void translate(glm::vec3 pos);
-    glm::mat4 get_view_mat();
+    const glm::mat4& get_view_mat();
     void rotate(float angle, glm::vec3 axis);
     void move_forward(float dist);
     void move_right(float dist);
