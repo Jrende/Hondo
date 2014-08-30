fc6e40d80cea53ee9eabb3d9e6042d5cde51fbb1
Johan Rende <johan@rende.se>
2014-08-31 00:30:00 +0200

    Assorted changes to main

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,24 @@
-#include <stdlib.h>
-#include <iostream>
-#include <stdio.h>
-#include <math.h>
-
+#define GLM_FORCE_RADIANS
+#define PI 3.14159265358979
 #include <GL/glew.h>
 #include <glfw3.h>
-
-#define GLM_FORCE_RADIANS
 #include <glm/glm.hpp>
 #include <glm/gtx/string_cast.hpp>
-#include <SOIL.h>
+#include <iostream>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 #include "gfx/Renderer.hpp"
-#include "gfx/importer/ObjLoader.hpp"
 #include "gfx/VertexArray.hpp"
+#include "gfx/importer/ObjLoader.hpp"
+#include "gfx/lights/Light.hpp"
+#include "gfx/lights/PointLight.hpp"
 #include "gfx/shader/SimpleShader.hpp"
-#include "input/Input.hpp"
+
 #include "input/Actions.hpp"
-#define PI 3.14159265358979
+#include "input/Input.hpp"
+#include "DebugUtils.h"
 
 static void error_callback(int error, const char* description)
 {
@@ -35,8 +36,8 @@ void rotate_camera() {
 float randx() {
   return (float) (rand() % 100)/100;
 }
-const int width = 640;
-const int height = 480;
+const int width = 1024;
+const int height = 768;
 int main(int argc, char ** argv) {
   glfwSetErrorCallback(error_callback);
   if (!glfwInit())
@@ -54,6 +55,8 @@ int main(int argc, char ** argv) {
 
   glewExperimental = GL_TRUE;
   GLenum err = glewInit();
+  //To discard error 1280 from glewInit().
+  glGetError();
   if(GLEW_OK != err) {
     std::cout << glewGetErrorString(err) << std::endl;
   }
@@ -86,11 +89,11 @@ int main(int argc, char ** argv) {
   });
 
   Input::on(Actions::Up, []() {
-    camera->translate({0, -step, 0});
+    camera->translate({0, step, 0});
   });
 
   Input::on(Actions::Down, []() {
-    camera->translate({0, step, 0});
+    camera->translate({0, -step, 0});
   });
 
   Input::on(GLFW_KEY_Z, [&] {
@@ -101,70 +104,70 @@ int main(int argc, char ** argv) {
       Input::lock_mouse();
   });
   ObjLoader loader;
-  //loader.preload("assets/sponza.obj");
   loader.preload("assets/Cube.obj");
-  //loader.preload("assets/Cube.obj");
+  loader.preload("assets/Floor.obj");
   loader.load_preloaded_data();
   auto vArrayPtr = std::shared_ptr<VertexArray>(new VertexArray(loader.vertex_array, loader.index_array, loader.vertex_count, {3, 2, 3}));
-  /*
-  int a,b,c;
-  a = b = c = 30;
-  std::cout << "Lets draw " << a*b*c << " cubes!" << std::endl;
-  for(int i = 0; i < a; i++) {
-    for(int j = 0; j < b; j++) {
-      for(int k = 0; k < c; k++) {
-	auto&& rObj1 = RenderObject(vArrayPtr, loader.mesh_list[0]);
-	rObj1.translate({i*3.0f, j*3.0f, k*3.0f});
-	rObj1.color = {1, 1, 1};
-	renderer.add_object(rObj1);
+
+  for(int i = -5; i < 5; i++) {
+    for(int j = -5; j < 5; j++) {
+      if((i % 2 == 0) || (j % 2 == 0)) {
+	const auto& cube = std::shared_ptr<RenderObject>(new RenderObject(vArrayPtr, loader.mesh_list[0]));
+	cube->translate({i*4.0f, 1.01f, j*4.0f});
+	renderer.add_object(cube);
       }
+      const auto& floor = std::shared_ptr<RenderObject>(new RenderObject(vArrayPtr, loader.mesh_list[1]));
+      floor->translate({i*4.0f, 0, j*4.0f});
+      floor->scale({2, 2, 2});
+      renderer.add_object(floor);
     }
   }
-  */
-
-  auto rObj1 = std::shared_ptr<RenderObject>(new RenderObject(vArrayPtr, loader.mesh_list[0]));
-  rObj1->translate({0, 0, 0});
-  rObj1->color = {1, 1, 1};
-  //renderer.add_object(rObj1);
-  std::cout << "(" << loader.mesh_list[0].start << ", " << loader.mesh_list[0].end << ")" << std::endl;
 
   /*
-  auto rObj2 = std::shared_ptr<RenderObject>(new RenderObject(vArrayPtr, loader.mesh_list[1]));
-  rObj2->translate({0, 0, 0});
-  rObj2->color = {1, 1, 1};
-  renderer.add_object(rObj2);
+  const auto& cube = std::shared_ptr<RenderObject>(new RenderObject(vArrayPtr, loader.mesh_list[0]));
+  cube->translate({0, 1.01, 0});
+  renderer.add_object(cube);
 
-  for(const auto& mesh: loader.mesh_list) {
-    auto rObj1 = std::shared_ptr<RenderObject>(new RenderObject(vArrayPtr, mesh));
-    renderer.add_object(rObj1);
-  }
+  const auto& floor = std::shared_ptr<RenderObject>(new RenderObject(vArrayPtr, loader.mesh_list[1]));
+  floor->scale({10, 1, 10});
+  renderer.add_object(floor);
   */
 
   renderer.set_camera(camera);
-  glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+  auto pl = std::shared_ptr<PointLight>(new PointLight({0,2,0}, {1,1,1}));
+  renderer.add_light(pl);
+
+  Input::on(GLFW_KEY_I, [&] {
+      pl->translate({ 0.01f, 0, 0});
+  }, true);
+  Input::on(GLFW_KEY_K, [&] {
+      pl->translate({-0.01f, 0, 0});
+  }, true);
+  Input::on(GLFW_KEY_J, [&] {
+      pl->translate({0, 0,  0.01f});
+  }, true);
+  Input::on(GLFW_KEY_L, [&] {
+      pl->translate({0, 0, -0.01f});
+  }, true);
+  Input::on(GLFW_KEY_O, [&] {
+      pl->translate({0,  0.01f, 0});
+  }, true);
+  Input::on(GLFW_KEY_U, [&] {
+      pl->translate({0,-0.01f, 0});
+  }, true);
 
   const int radius = 2;
   float i = 0;
+  camera->translate({0, 2, 0});
   while (!glfwWindowShouldClose(window)) {
     Input::handle_input();
     glfwPollEvents();
     rotate_camera();
 
     renderer.render();
-    for(int j = 0; j < 10; j++) {
-      for(int r = 0; r < 10; r++) {
-	for(int t = 0; t < 10; t++) {
-	  renderer.draw_point({j*2, r*2, t*2});
-	}
-      }
-    }
-    renderer.draw_line({0.1,0,0}, { 1,0,0});
-    renderer.draw_line({-0.1,0,0}, {-1,0,0});
-    renderer.draw_line({0,0.1,0}, {0, 1,0});
-    renderer.draw_line({0,-0.1,0}, {0,-1,0});
-    renderer.draw_line({0,0,0.1}, {0,0, 1});
-    renderer.draw_line({0,0,-0.1}, {0,0,-1});
-    //rObj1->set_position({sin(i * (PI/180)) * radius, 0, cos(i * (PI/180)) * radius});
+    renderer.draw_point(pl->pos);
+    renderer.draw_line({0, 0, 0}, {0, 1, 0});
+
     glfwSwapBuffers(window);
 
     Input::reset_delta();
@@ -172,8 +175,12 @@ int main(int argc, char ** argv) {
        i=0;
     }
     i += 0.1f;
+
+    calcFPS(window, 1.0, "Current FPS: ");
+    checkForGlError();
   }
   glfwDestroyWindow(window);
   glfwTerminate();
   exit(EXIT_SUCCESS);
 }
+
