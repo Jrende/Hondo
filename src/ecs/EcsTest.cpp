957417e352278d818dfd600c456bcc433ac905a2
jrende <johan@rende.se>
2015-08-05 20:22:20 +0200

    Used new allocate constructor in EcsTest

--- src/ecs/EcsTest.cpp
+++ src/ecs/EcsTest.cpp
@@ -4,6 +4,7 @@
 struct Test {
   int f;
   std::string s;
+  Test(std::string s, int f): s(s), f(f) {};
   //int s;
   //Test(): f(count++), s("") {}
 };
@@ -12,15 +13,11 @@ int main(int argc, char** argv) {
   Bag bag;
   bag.init<Test>();
   for(int i = 0; i < size; i++) {
-    Test* t = bag.allocate<Test>();
-    t->s = "it works!";
-    t->f = 100 + i;
+    Test* t = bag.allocate<Test>("it works!", 100 + i);
   }
 
   bag.free_obj(bag.get<Test>(1));
-  Test* t = bag.allocate<Test>();
-  t->s = "New item";
-  t->f = 200;
+  Test* t = bag.allocate<Test>("New item", 200);
   for(auto i = bag.begin<Test>(), end = bag.end<Test>(); i != end; i++) {
     const Test& t = *i;
     std::cout << t.f << ": " << t.s << std::endl;
