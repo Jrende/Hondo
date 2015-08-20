10b661ad70bc556ffa5025988d5e367f6c10cabe
jrende <johan@rende.se>
2015-08-20 20:18:42 +0200

    Changed comp map to array, removed ref comp caching

--- src/ecs/ComponentManager.hpp
+++ src/ecs/ComponentManager.hpp
@@ -2,6 +2,7 @@
 #include <unordered_map>
 #include <unordered_set>
 #include <list>
+#include <vector>
 #include "Component.hpp"
 #include "Bag.hpp"
 static int last_component_id = 0;
@@ -14,60 +15,45 @@ class ComponentManager {
     class Ref {
       friend ComponentManager;
       private:
-        bool is_dirty = true;
-        C* comp = nullptr;
-
         Entity entity;
-        Bag* bag;
-        ComponentManager* component_manager;
+        Bag* bag = nullptr;
       
       public:
-        Ref(Entity entity, Bag* bag, ComponentManager* component_manager): entity(entity), bag(bag), component_manager(component_manager) {
-          component_manager->references[C::type_id].insert(this);
+        Ref(Entity entity, Bag* bag): entity(entity), bag(bag) {
         };
 
-        ~Ref() {
-          component_manager->references[C::type_id].erase(this);
+        C* operator->() {
+          return bag->get_component_for_entity<C>(entity);
         }
 
-
-        C* operator->() {
-          if(is_dirty) {
-            comp = bag->get_component_for_entity<C>(entity);
-            is_dirty = false;
-          }
-          if(comp != nullptr) {
-            //Try not to do this only once every frame
-            component_manager->changed_components[C::type_id].insert(comp->get_id());
-          }
-          return comp;
+        bool operator==(const Ref& other) {
+          return other.entity.id == this->entity.id && other.component.id == this->component.id;
         }
     };
 
   private:
-    std::unordered_map<component_type, Bag> components;
-    std::unordered_map<component_type, std::unordered_set<int>> changed_components;
-    
-    // void* points to type of Ref<C>
-    std::unordered_map<component_type, std::unordered_set<void*>> references;
+    //Index of vector corresponds to component type id
+    std::vector<Bag> components;
+    std::vector<std::unordered_set<int>> changed_components;
     
     //ie entity_to_component_index[Transform::id][entity.id] will give the index of the entitys transform component
   public:
+    ComponentManager():
+      components(ComponentId::max_component_id),
+      changed_components(ComponentId::max_component_id)
+  {
+    //init refs set
+  }
+
     template<typename C, typename... Rest>
     void add_component(const Entity& e, Rest&&... args) {
       //Try using bag.at & exceptions, benchmark
-      if(components.count(Component<C>::type_id) == 0) {
+      if(components[Component<C>::type_id].size() == 0) {
         Bag& bag = components[Component<C>::type_id];
         bag.init<C>();
       }
 
       Bag& bag = components[Component<C>::type_id];
-
-      if(bag.will_resize<C>()) {
-        for(auto& ref: references[Component<C>::type_id]) {
-          static_cast<Ref<C>*>(ref)->is_dirty = true;
-        }
-      }
 
       C* c = bag.allocate<C>(e, std::forward<Rest>(args)...);
       c->id = last_component_id++;
@@ -76,11 +62,10 @@ class ComponentManager {
     template<typename C>
     ComponentManager::Ref<C> get_component_ref(const Entity& e) {
       Bag* bag = &components[Component<C>::type_id];
-      return ComponentManager::Ref<C>(Entity(e), bag, this);
+      return ComponentManager::Ref<C>(Entity(e), bag);
     }
 
     void clear() {
       changed_components.clear();
     }
-
 };
